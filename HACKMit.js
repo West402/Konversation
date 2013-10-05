0a18c3ec0e4ad4f8bfb3e0a042156b3ebd786ed9
Tyler Lubeck <tyler@tylerlubeck.com>
2013-10-05 19:54:03 -0400

    Cleaning code up. Hope you like rude commenting...

--- HACKMit.js
+++ HACKMit.js
@@ -1,60 +1,66 @@
+/* Basic set up stuff */
 var express = require("express");
 var escape = require('escape-html');
+var swig = require('swig');
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
-//app.engine('.html', require('ejs').renderFile);
-var swig = require('swig');
-
 app.use('/', express.static(__dirname + '/'));
 app.set('views', __dirname + '/views');
-//app.set('view_engine', 'ejs');
-
-
-var FACEBOOK_APP_ID = "728970310453334";
-var FACEBOOK_SECRET = "c83880e3888f67f60ca1369df27d688e";
-var graph = require('fbgraph');
-
 
 /*Auth Stuff Begin */
-var authBase = '/auth/facebook'
-var authRedirect = process.env.REDIRECT_URI || 'http://localhost:5000';
-authRedirect += authBase;
+	var authBase = '/auth/facebook'
+	var authRedirect = process.env.REDIRECT_URI || 'http://localhost:5000';
+	authRedirect += authBase;
+
+	var FACEBOOK_APP_ID = "728970310453334";
+	var FACEBOOK_SECRET = "c83880e3888f67f60ca1369df27d688e";
+	var graph = require('fbgraph');
+
+	// Essentially a struct for holding shit about facebook shit //
+	var conf = {
+		client_id: FACEBOOK_APP_ID,
+		client_secret: FACEBOOK_SECRET,
+		scope: 'read_mailbox',
+		redirect_uri: authRedirect
+	};
 /*Auth Stuff End */
 
-var conf = {
-	client_id: FACEBOOK_APP_ID,
-	client_secret: FACEBOOK_SECRET,
-	scope: 'read_mailbox',
-	redirect_uri: authRedirect
-};
-
 /* Session Storage Begin */
-var mongoUri = process.env.MONGOLAB_URI || 
-	process.env.MONGOHQ_URI ||
-	'mongodb://localhost/HACKMit_db';
+	var mongoUri = process.env.MONGOLAB_URI || 
+		process.env.MONGOHQ_URI ||
+		'mongodb://localhost/HACKMit_db';
 
-var MongoStore = require('connect-mongo')(express);
+	var MongoStore = require('connect-mongo')(express);
 
 
-app.use(express.cookieParser());
-app.use(express.session({
-	store: new MongoStore({
-		url: mongoUri
-	}),
-	secret: "avbpiubargu9badvar498vpbarv",
-	}));
-app.use(app.router);
+	app.use(express.cookieParser());
+	app.use(express.session({
+		store: new MongoStore({
+			url: mongoUri
+		}),
+		secret: "avbpiubargu9badvar498vpbarv",
+		}));
+	app.use(app.router);
 /*Session Storage End */
 
-var mongo = require('mongodb');
+
+/* Mongo shit. Dunno if we need it yet. Probably will in the future. So I leave it here.
+	var mongo = require('mongodb');
 
 
-var db = mongo.Db.connect(mongoUri, function(err, dbConnection) {
-	db = dbConnection;
-});
+	var db = mongo.Db.connect(mongoUri, function(err, dbConnection) {
+		db = dbConnection;
+	});
 
+ End of Mongo shit*/
 
+/* authBase is found above. I might change it later, so variables abound!
+ * This route is SO FREAKING IMPORTANT for accessing facebook shit.
+ * 
+ * 
+ * If you fuck with it I will find you.
+ */
 app.get(authBase, function(req, res) {
 	if(!req.query.code) {
 		var authUrl = graph.getOauthUrl({
@@ -82,6 +88,11 @@ app.get(authBase, function(req, res) {
 	});
 });
 
+
+/*Logout function. Pretty fucking simple. Learn to read.
+ * swear swear swear angst angst angst
+ * I need more coffee
+ */
 app.get('/logout', function(req, response){
 	req.session.destroy();
 	response.redirect('/');
@@ -92,11 +103,16 @@ app.get('/logout', function(req, response){
 
 app.get('/', function(req, response){
 
+	/* If there is a code in the session, they've logged in before */
 	if(req.session.code){
 		var query = "SELECT name FROM user WHERE uid = me()"
 		graph.setAccessToken(req.session.code);
 
 		graph.fql(query, function(err, res){
+			/* data being less then zero probably means they haven't logged in, 
+			 * which should be caught by the req.session.code above, but what the hell
+			 * amirite?
+			 */
 			if(res.data.length > 0) {
 				resStr = swig.renderFile('views/index.html', {name: res.data[0].name});
 			} else {
@@ -111,6 +127,8 @@ app.get('/', function(req, response){
 });
 
 
+
+/* Listening for stuff */
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
